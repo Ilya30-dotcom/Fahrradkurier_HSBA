d33bfa56e113f3acd30a725cdc92a7585a3fe801
Arne <arne.schroder@myhsba.de>
2021-10-27 11:11:12 +0200

    Created a new Spring project and added all important dependencies
    Initial Commit. Created new Spring Project and added all important dependencies

---
+++ src/main/java/de/hsba/bi/fahrradkurrier/FahrradkurrierApplication.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.fahrradkurrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahrradkurrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahrradkurrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsba/bi/fahrradkurrier/FahrradkurrierApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.fahrradkurrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahrradkurrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
