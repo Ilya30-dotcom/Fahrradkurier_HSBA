37c1461c357d36d9fe7402746c6146096bad656c
Ilja Nefjodov <93380468+LinLeyLux@users.noreply.github.com>
2021-11-07 17:37:50 +0100

    Adds error pages

---
+++ src/main/java/de/hsba/bi/fahrradkurrier/web/exception/ForbiddenException.java
@@ -0,0 +1,9 @@
+package de.hsba.bi.fahrradkurrier.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+
+}
