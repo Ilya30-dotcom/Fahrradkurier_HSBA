62ddfd7c760e6b18178f68be091bee647f6d4701
abdullah.bilal <abdullah.bilal@myhsba.de>
2021-12-12 15:01:55 +0100

    Added register.html to starting page.
    implemented registration form  successfully to the project.

---
+++ src/main/java/de/hsba/bi/fahrradkurrier/web/registration/RegistrationController.java
@@ -0,0 +1,41 @@
+package de.hsba.bi.fahrradkurrier.web.registration;
+
+import de.hsba.bi.fahrradkurrier.user.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.RequiredArgsConstructor;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private final UserService userService;
+    private final UserFormConverter userFormConverter;
+
+
+
+    @GetMapping()
+    public String home(Model model){
+        model.addAttribute("userForm", new UserForm());
+        return "register";
+    }
+
+
+
+    @PostMapping()
+    public String register(@ModelAttribute("userForm") @Valid UserForm userForm, BindingResult bindingResult) //, )
+    {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+       userService.save(userFormConverter.toEntity(userForm));
+        return "redirect:/";
+    }
+}
